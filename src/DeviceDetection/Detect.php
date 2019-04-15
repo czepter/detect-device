d039e902721f3dd024bd7fb5efe2a6a7d8e74f07
Christian <christian@czepter.de>
2019-04-15 18:53:52 +0200

    fix autoloading

---
+++ src/DeviceDetection/Detect.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace DeviceDetection;
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Detect.php';
+
+class Detect extends \Detect {}
